9f0b848f98bb45829cfa2026de0e868ff0e9dcec
eugene <kirnasuk1284@gmail.com>
2020-10-13 15:31:08 +0300

    create redux and modify redusers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,6 @@
+import dialogsReducer from "./dialogs-reducer";
+import profileReducer from "./profile-reducer";
+
 let store = {
     _state: {
         profilePage: {
@@ -5,7 +8,7 @@ let store = {
                 {id: 1, message: 'Hi, how are you?', likesCount: 15},
                 {id: 2, message: 'This is my second post', likesCount: 23},
                 {id: 3, message: 'Hi everyone!', likesCount: 67}],
-            newPostText: '12'
+            newPostText: ''
         },
         messagePage: {
             dialogsData: [
@@ -19,7 +22,8 @@ let store = {
                 {id: 2, message: 'yo'},
                 {id: 3, message: 'How are you?'},
                 {id: 4, message: 'Do you know me?'},
-                {id: 5, message: 'By by'}]
+                {id: 5, message: 'By by'}],
+            newMessageBody:''
         }
     },
     _callSubscriber() {
@@ -35,28 +39,15 @@ let store = {
     // або через приват методи визиваючи їх в dispatch
 
     dispatch(action) {
-        debugger;
-        if (action.type === 'ADD-POST') {
-            let newPost = {
-                id: 5,
-                message: this._state.profilePage.newPostText,
-                likesCount: 0
-            };
-            this._state.profilePage.postsData.unshift(newPost);
-            this._state.profilePage.newPostText = '';
-            this._callSubscriber(this._state);
-        } else if (action.type === 'UPDATE-NEW-POST-TEXT') {
-            this._state.profilePage.newPostText = action.newText;
-            this._callSubscriber(this._state)
-        }
-        else {
-            console.log('missing despatch')
-        }
+        this._state.profilePage = profileReducer(this._state.profilePage,action)
+        this._state.messagePage = dialogsReducer(this._state.messagePage,action)
 
-
+        this._callSubscriber(this._state)
     }
-
-
 }
+
+
+
+
 
 export default store;
